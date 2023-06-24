a15d981732df0bddce28774a26e6d8a5dfe9386a
Kuranai <mail@kuranai.de>
2023-06-24 15:49:24 +0200

    new project setup

---
+++ README.md
@@ -0,0 +1,17 @@
+# domainhacks
+
+small project to search for domain hacks like sear.ch, engi.ne, vie.ws and so on
+
+## How to use
+- put some tlds.txt and words.txt file in the data directory
+- run GenerateDomainHacks.py
+- Check domain availability with CheckDomains.py 
+
+## Development
+This project uses [Rye](https://rye-up.com/) 
+
+Installation guide: https://rye-up.com/guide/installation/
+
+When Rye is installed:
+- git clone the project
+- rye sync
---
+++ src/domainhacks/GenerateDomainHacks.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+
+script_dir = Path(__file__).parent
+tlds_path = (script_dir / "../../data/tlds.txt").resolve()
+words_path = (script_dir / "../../data/words.txt").resolve()
+output_path = (script_dir / "../../data/output.txt").resolve()
+
+tlds = []
+with open(tlds_path, "r") as f:
+    for line in f:
+        tlds.append(line.strip().lower())
+
+words = []
+with open(words_path, "r") as f:
+    for line in f:
+        words.append(line.strip())
+
+with open(output_path, "w") as f:
+    for word in words:
+        for tld in tlds:
+            if word.endswith(tld) and word != tld:
+                f.write(f"{word[:-len(tld)]}.{tld}\n")
