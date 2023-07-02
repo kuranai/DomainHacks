82a8c2d1a60f666112c2de074105cca821bae1bc
Kuranai <mail@kuranai.de>
2023-07-02 10:18:18 +0200

    set encoding to utf-8

--- src/domainhacks/GenerateDomainHacks.py
+++ src/domainhacks/GenerateDomainHacks.py
@@ -7,12 +7,12 @@ words_path = (script_dir / "../../data/words.txt").resolve()
 output_path = (script_dir / "../../data/output.txt").resolve()
 
 tlds = []
-with open(tlds_path, "r") as f:
+with open(tlds_path, "r", encoding="UTF-8") as f:
     for line in f:
         tlds.append(line.strip().lower())
 
 words = []
-with open(words_path, "r") as f:
+with open(words_path, "r", encoding="UTF-8") as f:
     for line in f:
         words.append(line.strip())
 
